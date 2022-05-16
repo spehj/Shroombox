d1268723348422c022e4698ad7fa029913c4d533
msedej96 <matic.m.sedej@gmail.com>
2022-05-16 21:25:18 +0200

    display_values

--- lib/Icons_16x16/Icons_16x16.h
+++ lib/Icons_16x16/Icons_16x16.h
@@ -1,9 +1,9 @@
 #ifndef Icons_16x16_h
 #define Icons_16x16_h
 
-		const unsigned char signal1_icon16x16;
-		const unsigned char signal2_icon16x16;
-		const unsigned char signal3_icon16x16;
-		const unsigned char signal4_icon16x16;
+	const unsigned char signal1_icon16x16;
+	const unsigned char signal2_icon16x16;
+	const unsigned char signal3_icon16x16;
+	const unsigned char signal4_icon16x16;
 
 #endif
