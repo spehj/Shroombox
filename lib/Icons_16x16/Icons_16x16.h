62358ef73feb74bea88aea535c0e858e52fb200a
msedej96 <matic.m.sedej@gmail.com>
2022-05-16 21:17:30 +0200

    display_values

---
+++ lib/Icons_16x16/Icons_16x16.h
@@ -0,0 +1,70 @@
+/*
+Icon Set Name: IoT Icon set 16x16 bi color
+Version: 1.0.0
+Origin URL:  http://engsta.com/iot-icon-set-for-i2c-oled-displays/
+Author: Artur Funk
+License: GNU General Public License v3
+*/
+
+#ifndef __icon_set_H__
+#define __icon_set_H__
+
+		const unsigned char home_icon16x16;
+		const unsigned char arrow_up_icon16x16;
+		const unsigned char arrow_down_icon16x16;
+		const unsigned char arrow_left_icon16x16;
+		const unsigned char arrow_right_icon16x16;
+		const unsigned char bat0_icon16x16;
+		const unsigned char bat1_icon16x16;
+		const unsigned char bat2_icon16x16;
+		const unsigned char bat3_icon16x16;
+		const unsigned char bat4_icon16x16;
+		const unsigned char bluetooth_icon16x16;
+		const unsigned char bulb_icon16x16;
+		const unsigned char bulb_on_icon16x16;
+		const unsigned char bulb_off_icon16x16;
+		const unsigned char bullet_icon16x16;
+		const unsigned char cancel_icon16x16;
+		const unsigned char check_icon16x16;
+		const unsigned char fillstate1_icon16x16;
+		const unsigned char fillstate2_icon16x16;
+		const unsigned char fillstate3_icon16x16;
+		const unsigned char fillstate4_icon16x16;
+		const unsigned char speak_icon16x16;
+		const unsigned char heart_icon16x16;
+		const unsigned char nocon_icon16x16;
+		const unsigned char tool_icon16x16;
+		const unsigned char plug_icon16x16;
+		const unsigned char powerbutton_icon16x16;
+		const unsigned char tool_icon16x16;
+		const unsigned char plug_icon16x16;
+		const unsigned char powerbutton_icon16x16;
+		const unsigned char wallplug_icon16x16;
+		const unsigned char wifi1_icon16x16;
+		const unsigned char wifi2_icon16x16;
+		const unsigned char clock_icon16x16;
+		const unsigned char timer_icon16x16;
+		const unsigned char water_tap_icon16x16;
+		const unsigned char humidity_icon16x16;
+		const unsigned char humidity2_icon16x16;
+		const unsigned char sun_icon16x16;
+		const unsigned char temperature_icon16x16;
+		const unsigned char noninverted_icon16x16;
+		const unsigned char inverted_icon16x16;
+		const unsigned char lock_closed_icon16x16;
+		const unsigned char lock_open_icon16x16;
+		const unsigned char person_icon16x16;
+		const unsigned char window_icon16x16;
+		const unsigned char door_icon16x16;
+		const unsigned char face_icon16x16;
+		const unsigned char siren_icon16x16;
+		const unsigned char warning_icon16x16;
+		const unsigned char plus_icon16x16;
+		const unsigned char minus_icon16x16;
+		const unsigned char mobile_icon16x16;
+		const unsigned char signal1_icon16x16;
+		const unsigned char signal2_icon16x16;
+		const unsigned char signal3_icon16x16;
+		const unsigned char signal4_icon16x16;
+
+#endif
